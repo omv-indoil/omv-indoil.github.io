153555cf7fc7af761c70a3650737f925ebdbc247
tpetkovic <tomislav.petkovic2@fer.hr>
2020-04-09 18:45:04 +0200

    first commit

---
+++ precache-manifest.776efc4bd7e434cedfafacd60d778e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ca3b5255bb88cf96d06d95da9821ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb703c0b2cdfd097e46d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "428d5dce53d41e8f2d69",
+    "url": "/static/css/main.374137a0.chunk.css"
+  },
+  {
+    "revision": "bb703c0b2cdfd097e46d",
+    "url": "/static/js/2.d5f0fd06.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/static/js/2.d5f0fd06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428d5dce53d41e8f2d69",
+    "url": "/static/js/main.a3916683.chunk.js"
+  },
+  {
+    "revision": "34ddccf364374fd8e59b",
+    "url": "/static/js/runtime-main.443ca74d.js"
+  }
+]);
